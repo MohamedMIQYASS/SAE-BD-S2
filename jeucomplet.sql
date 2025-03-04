371844519e23eab224d9be4a766802c36dc29e98
MohamedMIQYASS <mohamed.miqyass1@etu.univ-orleans.fr>
2025-03-04 11:36:47 +0100

    legere modif

--- jeucomplet.sql
+++ jeucomplet.sql
@@ -13057,7 +13057,7 @@ insert into POSSEDER(idmag, isbn, qte) values
 	(1, '9782226085962', 10),
 	(1, '9782081271654', 9),
 	(7, '9782070570669', 6),
-	(7, '9782844273765' , 3);
+	(7, '9782844273765' ,3);
 
 insert into COMMANDE(numcom, datecom, enligne, livraison, idcli, idmag) values
 	(1,str_to_date('1/1/2020','%d/%m/%Y'),'N','M',356,5),
