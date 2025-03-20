9da9d900428bf11725d70ccd6240ee4289c08f04
MohamedMIQYASS <mohamed.miqyass1@etu.univ-orleans.fr>
2025-03-20 15:57:35 +0100

    fichier python maths

---
+++ analayse_donnees.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+ # requete : select MONTH(datecom) as mois , count(numcom) as nbr, sum(qte*prixvente) as CA from LIVRE natural join DETAILCOMMANDE natural join COMMANDE group by mois;
+
